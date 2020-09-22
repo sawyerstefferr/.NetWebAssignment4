fc11e7d9bb97797d76b66d748562f0377246e549
Sawyer Steffen <sawyerstefferr@gmail.com>
2020-09-21 21:49:08 -0500

    Submission

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Category() => View(repository.Categories.OrderBy(c => c.CategoryName));
+
+    }
+}
